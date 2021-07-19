cc17e74327638654e794096a622b92b8cf5e2ca9
Sean Yap <syap0317@gmail.com>
2021-07-19 13:19:32 -0400

    interpolated inline styles represented by JS objects in JSX

--- src/05.js
+++ src/05.js
@@ -1,20 +1,33 @@
 // Styling
 // http://localhost:3000/isolated/exercise/05.js
 
-import * as React from 'react'
-import '../box-styles.css'
+import * as React from "react";
+import "../box-styles.css";
 
-// 💰 Use the className for the size and style (backgroundColor) for the color
-// 💰 each of the elements should also have the "box" className applied
-
-// 🐨 add a className prop to each of these and apply the correct class names
-// 💰 Here are the available class names: box, box--large, box--medium, box--small
-
-// 🐨 add a style prop to each of them as well so their background color
-// matches what the text says it should be as well as `fontStyle: 'italic'`
-const smallBox = <div>small lightblue box</div>
-const mediumBox = <div>medium pink box</div>
-const largeBox = <div>large orange box</div>
+const smallBox = (
+  <div
+    className="box box--small"
+    style={{ fontStyle: "italic", backgroundColor: "lightblue" }}
+  >
+    small lightblue box
+  </div>
+);
+const mediumBox = (
+  <div
+    className="box box--medium"
+    style={{ fontStyle: "italic", backgroundColor: "pink" }}
+  >
+    medium pink box
+  </div>
+);
+const largeBox = (
+  <div
+    className="box box--large"
+    style={{ fontStyle: "italic", backgroundColor: "orange" }}
+  >
+    large orange box
+  </div>
+);
 
 function App() {
   return (
@@ -23,7 +36,7 @@ function App() {
       {mediumBox}
       {largeBox}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
