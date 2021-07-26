de59097c6360dbda705a313b2c46fc6f7a84218d
Sean Yap <syap0317@gmail.com>
2021-07-26 17:51:14 -0400

    colocated state to the child component that uses it

--- react-hooks/03.js
+++ react-hooks/03.js
@@ -1,16 +1,26 @@
 // Lifting state
+// http://localhost:3000/isolated/exercise/03.js
 
 import * as React from "react";
 
-function Name({ name, onNameChange }) {
+// this name component encapsulates its state, hiding the implementation details
+// away from user of this component
+function Name() {
+  const [name, setName] = React.useState("");
   return (
     <div>
       <label htmlFor="name">Name: </label>
-      <input id="name" value={name} onChange={onNameChange} />
+      <input
+        id="name"
+        value={name}
+        onChange={(event) => setName(event.target.value)}
+      />
     </div>
   );
 }
 
+// we are passing the state managed by the parent into this component as props
+// this makes the API of this component slightly more complicated
 function FavoriteAnimal({ animal, onAnimalChange }) {
   return (
     <div>
@@ -20,24 +30,30 @@ function FavoriteAnimal({ animal, onAnimalChange }) {
   );
 }
 
-function Display({ name, animal }) {
-  return <div>{`Hey ${name}, your favorite animal is: ${animal}!`}</div>;
+function Display({ animal }) {
+  return <div>{`Your favorite animal is: ${animal}!`}</div>;
 }
 
 function App() {
-  const [name, setName] = React.useState("");
+  // whenever you see states being managed in a component, always determine
+  // what elements in this component are using this state
+  // if you notice only one element in this component is using this state,
+  // it is a sign for you to "colocate" or move the state management into the element
+  // this would make the API of the component easier to use and it can encapsulate it own state by itself
   const [animal, setAnimal] = React.useState("");
 
+  // if we are managing state in a parent component, we will need to pass the
+  // state updater function into the element using the state (as seen with setAnimal)
+
+  // FavoriteAnimal & Display are siblings component that require the shared state
   return (
     <form>
-      <Name name={name} onNameChange={(event) => setName(event.target.value)} />
-
+      <Name />
       <FavoriteAnimal
         animal={animal}
         onAnimalChange={(event) => setAnimal(event.target.value)}
       />
-
-      <Display name={name} animal={animal} />
+      <Display animal={animal} />
     </form>
   );
 }
