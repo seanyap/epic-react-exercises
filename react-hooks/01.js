0334d8732f603da9df2a069f261b425c9e9c600e
Sean Yap <syap0317@gmail.com>
2021-08-04 23:52:42 -0400

    updated comments to improve explanation

--- react-hooks/01.js
+++ react-hooks/01.js
@@ -1,23 +1,25 @@
 // useState: greeting
-// http://localhost:3000/isolated/exercise/01.js
 
 import * as React from "react";
 
+// Hooks = functions that let you "hook" into React state and lifecycle features
+//         from function components
+// State = data that change over time
+
 function Greeting({ initialName }) {
   // use a hook to hook into(add) react state into the instance of your function component
-  // useState hook gives you a function to update the state, which will trigger the re-render
-  // in the re-render, useState hook will give you the state value for the current render(the value you called setState with)
+  // useState hook returns an array which contains an updater function to update the state
+  // calling the updater function will trigger a re-render
+  // useState will return the new state value you called setState with
   const [name, setName] = React.useState(initialName);
-
-  // state = data that change over time
 
   function handleChange(event) {
     // update the state "name" based on event.target.value (user input)
     setName(event.target.value);
   }
 
-  // Greeting function is called again to re-render(return) an updated UI when setState is called
-  // if we don't use setState, then the Greeting func will not be called again, only once when it initialy renders
+  // when setState is called, Greeting function component is called again to re-render(return) an updated UI
+  // if we don't use setState, then Greeting func will not be called again, only once when it initialy renders
   // since func has its own local scope, and when it returns its local var will be gone
   // we need a way to re-run this func multiple times while keeping the "name" state
   // so we can display the state value on our UI
